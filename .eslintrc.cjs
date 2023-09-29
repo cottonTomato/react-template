dc330fc67d71e3aa547b6e10fc0508385f810de5
LanceHoenhymm <abhianv.pandey.1512@gmail.com>
2023-09-29 14:34:44 +0530

    Initial Commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended-type-checked',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'prettier',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+    },
+    plugins: ['@typescript-eslint', 'react', 'react-hooks', 'react-refresh'],
+    rules: {
+        'react/react-in-jsx-scope': 'off',
+        'react-refresh/only-export-components': [
+            'warn',
+            { allowConstantExport: true },
+        ],
+        '@typescript-eslint/no-unused-vars': [
+            'warn',
+            {
+                argsIgnorePattern: '^_',
+                varsIgnorePattern: '^_',
+            },
+        ],
+    },
+};
